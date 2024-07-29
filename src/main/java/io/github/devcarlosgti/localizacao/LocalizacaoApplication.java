0149e92259f6998ae23f865fdfd97f75d84bbe96
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-07-29 09:20:51 -0300

    Creada a classe CidadeRepository e Testando

--- src/main/java/io/github/devcarlosgti/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/devcarlosgti/localizacao/LocalizacaoApplication.java
@@ -1,15 +1,36 @@
 package io.github.devcarlosgti.localizacao;
 
+import io.github.devcarlosgti.localizacao.domain.entity.Cidade;
+import io.github.devcarlosgti.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Inicializado");
+		//System.out.println("Inicializado");
+		//testes temporarios..
+		salvarCidade();
+		listarCidade();
+	}
+
+	//testando cidade no DB
+	@Transactional // transação de DB
+	void salvarCidade(){
+		var cidade = new Cidade(1L, "São Luís", 1779000L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidade(){
+		cidadeRepository.findAll().forEach(System.out::println);
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
